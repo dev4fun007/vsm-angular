90d90be979105edba2a90e1853c0586f6a717b8e
lordprat <44286985+lordprat@users.noreply.github.com>
2018-10-22 16:56:21 +0530

    Adding configurable header and correcting fonts location

--- src/app/modules/shared/components/tile/tile.component.ts
+++ src/app/modules/shared/components/tile/tile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tile',
@@ -8,9 +9,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TileComponent implements OnInit {
 
   @Input() data;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateToDepartmentPage(data) {
+    this.router.navigateByUrl('/department?org=' + data.orgName + '&scope=' + data.scope);
   }
 
 }
